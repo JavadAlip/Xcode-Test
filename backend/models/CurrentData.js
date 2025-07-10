604cd7a5cb364f152b2904d4f429bacc275ebaef
JavadAlip <javadalipp911@gmail.com>
2025-07-10 18:27:58 +0530

    Code aligned

--- backend/models/CurrentData.js
+++ backend/models/CurrentData.js
@@ -1,18 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const currentDataSchema = new mongoose.Schema({
-//   coinId: String,
-//   name: String,
-//   symbol: String,
-//   price: Number,
-//   marketCap: Number,
-//   change24h: Number,
-//   timestamp: Date
-// });
-
-// module.exports = mongoose.model("CurrentData", currentDataSchema);
-
-
 const mongoose = require("mongoose");
 
 const currentDataSchema = new mongoose.Schema({
--- backend/models/HistoryData.js
+++ backend/models/HistoryData.js
@@ -1,19 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const HistoryDataSchema = new mongoose.Schema({
-//   coinId: String,
-//   name: String,
-//   symbol: String,
-//   price: Number,
-//   timestamp: {
-//     type: Date,
-//     default: Date.now
-//   }
-// });
-
-// module.exports = mongoose.model("HistoryData", HistoryDataSchema);
-
-
 const mongoose = require("mongoose");
 
 const historyDataSchema = new mongoose.Schema({
