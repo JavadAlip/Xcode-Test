37d609d182f95e278c504897b1b010d128fb70e0
JavadAlip <javadalipp911@gmail.com>
2025-07-11 11:57:25 +0530

    new

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:5000/api/coins"; // Adjust if needed
+// const BASE_URL = "http://localhost:5000/api/coins"; // Adjust if needed
+const BASE_URL = `${import.meta.env.VITE_API_URL}/coins`;
 
 export const fetchCoins = async () => {
   const res = await fetch(BASE_URL);
